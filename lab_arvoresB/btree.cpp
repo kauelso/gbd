09fad1d17f0f4b89b31dc5f93b8f698600139d00
Guilherme Berti <guilhermecberti@gmail.com>
2021-10-19 18:53:48 -0300

    split e insercao na folha

--- lab_arvoresB/btree.cpp
+++ lab_arvoresB/btree.cpp
@@ -40,7 +40,70 @@ int btree::insereRecursao(int chave, int indicePg){
     //pagina nao cheia
         if(pg->ponteiros[0] == -1){ //Caso o primeiro dado dos ponteiros seja -1, o noh eh folha
             if(pg->numeroElementos == ORDEM){
-                //Dividir nó e inserir o elemento
+                //Dividir no e inserir o elemento
+                //o elemento do meio
+                int elementomeio = pg->chaves[(ORDEM-1)/2];
+
+                // cria��o de 1 nova pagina
+                int indice3;
+                //pagina *novaPagina1 = novaPagina(indice3);
+
+                //metade das chaves na pagina nova
+                for(int i=0;i<=(ORDEM-1)/2;i++){
+                    novaPagina1->chaves[i] = pg->chaves[i];
+                    novaPagina1->numeroElementos++;
+                }
+
+                //removendo todas chaves que foram transferidas
+                for(int i=0;i<=(ORDEM-1)/2;i++){
+                    for(int j=0;j<=pg->numeroElementos;j++){
+                        pg->chaves[j] = pg->chaves[j+1];
+                    }
+                }
+                pg->numeroElementos = (ORDEM-1)/2;
+                // fim da divisao
+
+
+                // insercao na nova
+                if(chave < novaPagina1->chave[novaPagina1->numeroElementos-1]){
+                    *pg = *novaPagina1;     //trato novapagina1 como pg para facilitar o codigo
+                    // igual inserir em folha vazia
+                    int i = pg->numeroElementos-1;
+                    if(chave > pg->chaves[i]){ // e maior que o ultimo valor da pagina? entao insere no final
+                        pg->chaves[i+1] = chave;
+                    }
+                    else{   //se n�o move todos para frente ate colocar na posicao correta
+                        while(pg->chaves[i] > chave){
+                            pg->chaves[i] = pg->chaves[i-1];
+                            i--;
+                        }
+                        pg->chaves[i+1] = chave;
+                    }
+                    pg->numeroElementos+1;
+                }
+                //insercao na ja existente
+                else{
+                    // igual inserir em folha vazia
+                    int i = pg->numeroElementos-1;
+                    if(chave > pg->chaves[i]){ // � maior que o ultimo valor da pagina? ent�o insere no final
+                        pg->chaves[i+1] = chave;
+                    }
+                    else{   //se n�o move todos para frente at� colocar na posi��o correta
+                        while(pg->chaves[i] > chave){
+                            pg->chaves[i] = pg->chaves[i-1];
+                            i--;
+                        }
+                        pg->chaves[i+1] = chave;
+                    }
+                    pg->numeroElementos+1;
+                }
+
+                
+
+
+
+
+
             }
             int i = pg->numeroElementos-1;
             if(chave > pg->chaves[i]){ // e maior que o ultimo valor da pagina? entao insere no final
